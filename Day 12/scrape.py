b3db6493858adecaaa40737e1a6f7aeaab377c69
danjuma1 <hda0202@gmail.com>
2021-03-20 01:46:47 +0100

    worked on day 12

---
+++ Day 12/scrape.py
@@ -0,0 +1,28 @@
+import datetime
+import requests
+from requests_html import HTML
+
+now = datetime.datetime.now()
+year = now.year
+
+def url_to_txt(url, filename="world.html", save=False):
+    r = requests.get(url)
+    if r.status_code == 200:
+        html_text = r.text
+        if save:
+            with open(f"world-{year}.html", 'w') as f:
+                f.write(html_text)
+        return html_text
+    return ""
+
+url = "https://www.boxofficemojo.com/year/world/"
+
+html_text = url_to_txt(url)
+
+r_html = HTML(html=html_text)
+table_class = ".imdb-scroll-table"
+r_table = r_html.find(table_class)
+
+# print(r_table)
+if len(r_table) == 1:
+    print(r_table[0].text)
